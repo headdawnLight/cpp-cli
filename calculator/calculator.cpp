dd4599689060a4c68a9a9ab3bea36af42dd91a98
Sertan <sertanix@yandex.com>
2021-05-25 23:49:35 +0300

    Add New File

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -2,16 +2,41 @@
 // calculator.cpp
 #include <iostream>
 using namespace std;
+
 int main()
 {
-	int num1;
-	int num2;
-	int sum;
-	cout << "Enter first number:\n";
-	cin >> num1;
-	cout << "Enter second number:\n";
-	cin >> num2;
-	sum = num1 + num2;
-	cout << "The sum of two numbers: " << sum << endl;
+	char op;
+	double num1,num2;
+
+	cout << "Enter the Operator ( +, -, *, / ) : ";
+	cin >> op;
+
+	cout << "Enter two numbers one by one : ";
+	cin >> num1 >> num2;
+
+	switch (op) {
+
+		case '+':
+			cout << num1 << '+' << num2 << '=' << (num1 + num2);
+			break;
+
+		case '-':
+			cout << num1 << '-' << num2 << '=' << (num1 - num2);
+			break;
+
+		case '*':
+			cout << num1 << '*' << num2 << '=' << (num1 * num2);
+			break;
+
+		case '/':
+			if (num2 != 0.0)
+				cout << num1 << '/' << num2 << '=' << (num1 / num2);
+			else
+				cout << "Divide by zero situation";
+			break;
+
+		default:
+			cout << op << "is an invalid operator";
+}
 	return 0;
 }
