98bda33cbf082e38225de292cae0bdfc558857ad
Sertan Işık <82842186+headdawnLight@users.noreply.github.com>
2021-06-30 23:56:52 +0300

    Update calculator.cpp

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -5,23 +5,39 @@ using namespace std;
 
 int main()
 {
-	double num1, num2, sum;
 	char op;
-	do {
-		cout << "\nEnter first number: ";
-		cin >> num1;
-		cout << "Enter the operator (+) (-) (*) (/) : ";
-		cin >> op;
-		cout << "Enter second number: ";
-		cin >> num2;
-		
-		if (op=='+') sum=num1+num2;
-		if (op=='-') sum=num1-num2;
-		if (op=='*') sum=num1*num2;
-		if (op=='/') sum=num1/num2;
-		
-		cout << "Calculation result: " << sum << endl; 
+	double num1,num2;
+
+	cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the operator (+) (-) (*) (/) : ";
+	cin >> op;
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	switch (op) {
+
+		case '+':
+			cout << num1 << '+' << num2 << '=' << (num1 + num2);
+			break;
+
+		case '-':
+			cout << num1 << '-' << num2 << '=' << (num1 - num2);
+			break;
+
+		case '*':
+			cout << num1 << '*' << num2 << '=' << (num1 * num2);
+			break;
+
+		case '/':
+			if (num2 != 0.0)
+				cout << num1 << '/' << num2 << '=' << (num1 / num2);
+			else
+				cout << "Divide by zero situation";
+			break;
+
+		default:
+			cout << op << "is an invalid operator";
 }
-	while (op!='e');
 	return 0;
 }
