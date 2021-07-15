8434d06cae32133d2ab600bf91c60f9d9efc1f7e
Sertan Işık <sertanix@yandex.com>
2021-07-15 23:20:28 +0300

    Revert "Update calculator.cpp"
    This reverts commit 28c9d26a3485e2f97b403a2178b403f66ac198d3.

--- calculator.cpp
+++ calculator.cpp
@@ -1,14 +1,13 @@
-// Calculator Programming with C++ on MS Visual Studio 2019
+// Calculator Programming with C++
 // calculator.cpp
 #include <iostream>
 using namespace std;
 
 int main()
 {	
-	//A simple calculation requires two operands and an operation
 	char op;
 	double num1, num2;
-
+	
 	cout << "\nEnter the first number: ";
 	cin >> num1;
 	cout << "Enter the operator (+) (-) (*) (/) : ";
@@ -16,50 +15,34 @@ int main()
 	cout << "Enter the second number: ";
 	cin >> num2;
 
-	//Addition, subtraction, multiplication and division operations
 	switch (op) {
 
 		case '+':
-			cout << "\nThe result of addition: "
-				<< num1 << '+' << num2 << '=' << (num1 + num2) << endl;
+			cout << "The result of addition: " 
+				<< num1 << '+' << num2 << '=' << (num1 + num2);
 		break;
 
 		case '-':
-			cout << "\nThe result of subtraction: "
-				<< num1 << '-' << num2 << '=' << (num1 - num2) << endl;
+			cout << "The result of subtraction: " 
+				<< num1 << '-' << num2 << '=' << (num1 - num2);
 		break;
 
 		case '*':
-		    cout << "\nThe result of multiplication: "
-				<< num1 << '*' << num2 << '=' << (num1 * num2) << endl;
+		    cout << "The result of multiplication: " 
+				<< num1 << '*' << num2 << '=' << (num1 * num2);
 		break;
 
 		case '/':
 
 		if (num2 != 0.0)
-			cout << "\nThe result of division: " 
-				<< num1 << '/' << num2 << '=' << (num1 / num2) << endl;
+			cout << "The result of division: " 
+				<< num1 << '/' << num2 << '=' << (num1 / num2);
 		else 
-			cout << "\nAn invalid transaction" << endl;
+			cout << "an invalid transaction";
 		break;
 
 		default: 
-			cout << op << " is an invalid operator" << endl;
-	}
-
-	//prompt to perform another calculation
-	cout << "\nPress 'C' key to perform another calculation or press 'E' key to exit: " << flush;
-	char redo;
-	cin >> redo;
-	switch (redo) {
-
-		case 'C': case 'c':
-		main();
-		break;
-
-		case 'E': case 'e':
-		cout << "\nDeveloped by headdawnLight" << endl;
-		break;
+			cout << op << " is an invalid operator";
 	}
 	return 0;
 }
